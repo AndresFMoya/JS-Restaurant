c9846a25d2644259ef076d5b3ccdf9d493db5eca
AndresFMoya <andresfmoya@gmail.com>
2019-12-17 16:53:54 -0500

    feat: add menu section

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import './styles/index.scss';
 import renderHeader from './components/header';
 import renderAbout from './components/about';
+import renderMenu from './components/menu';
 
 function init() {
   console.log("Hello");
   renderHeader();
-  renderAbout()
+  renderAbout();
+  renderMenu();
 }
 
 init();
