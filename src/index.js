4a178021c4b5b86dd558ff0c10b238ef6fc83cc0
AndresFMoya <andresfmoya@gmail.com>
2019-12-17 16:57:51 -0500

    feat: add contact section

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import './styles/index.scss';
 import renderHeader from './components/header';
 import renderAbout from './components/about';
 import renderMenu from './components/menu';
+import renderContact from './components/contact';
 
 function init() {
   console.log("Hello");
   renderHeader();
   renderAbout();
   renderMenu();
+  renderContact()
 }
 
 init();
